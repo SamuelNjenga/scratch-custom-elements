55f9e9d1f31bc98dc3f7d898f88da88561e1552e
Samuel Njenga <snjenga704@gmail.com>
2023-03-07 16:29:58 +0300

    feat: include shadow DOM for the h3 header

--- ActivityItem.js
+++ ActivityItem.js
@@ -1,9 +1,36 @@
+// Create template
+const template = document.createElement("template");
+
+// template.innerHTML = `
+//   <style>
+//    h3 { color: green}
+//   </style>
+//   <h3 data-title></h3>
+//   `;
+
+template.innerHTML = `
+  <style>
+   h3 { color: green}
+  </style>
+  <h3>
+  <slot></slot>
+  </h3>
+  `;
+
 // Create custom HTML Element
 class ActivityItem extends HTMLElement {
   constructor() {
     super(); // Makes sure we calling HTMLElement constructor
     // set up our todo item
-    this.innerHTML = "Hi I am custom";
+    // this.innerHTML = "Hi I am custom";
+    // this.innerHTML = `<h3>${this.innerText}</h3>`;
+    const shadow = this.attachShadow({ mode: "open" });
+    // Make sure you clone everything inside of the template
+    shadow.append(template.content.cloneNode(true));
+    // Get anything inside of our shadow
+    // this.title = shadow.querySelector("[data-title]")
+    // this.title.innerText = this.innerText
+    // this.innerHTML = `<style>h3{color:green}</style><h3>${this.innerText}</h3>`;
   }
 }
 // Register custom element with the dom
